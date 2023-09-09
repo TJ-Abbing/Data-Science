432ed2115378c2583c48d01ec9746a15e1d1e588
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-09 14:58:11 +0200

    Updated; assignment-1 code structure.

--- week-2/assignments/assignment-1.py
+++ week-2/assignments/assignment-1.py
@@ -8,6 +8,10 @@ if age < 18 or age > 70:
 
     print("Sorry, for security reasons we can't sell you a ticket.")
 
+    # If user is older than 25, ticket price is €15.
+elif age >= 25:
+    print('Ticket price: €15')
+
     # Check if user is younger than 25. 
 elif age < 25:
 
@@ -15,8 +19,12 @@ elif age < 25:
     is_student = input("Are you a student? (Enter 'yes' or 'no'): ").lower() == 'yes'
     has_membership = input("Do you have a membership card? (Enter 'yes' or 'no'): ").lower() == 'yes'
 
+    # Check if false (=not a student and not a member). If false, ticket price is €10.
+    if not is_student and not has_membership:
+        print('Ticket price: €10')
+
     # Check if user is both a student and member. 
-    if is_student and has_membership:
+    elif is_student and has_membership:
 
         # If true, check if user's age is an odd number. If true (=odd number), give user a free ticket.
         if age % 2 == 1:
@@ -25,11 +33,3 @@ elif age < 25:
         # If false (=even number), check if user is either a student or a member. If true, ticket price is €5.
         elif is_student or has_membership:
             print('Ticket price: €5')   
-
-        # If false (=not a student and not a member), ticket price is €10.
-        elif not is_student and not has_membership:
-            print('Ticket price: €10')
-
-# If user is older than 25, ticket price is €15.
-elif age > 25:
-    print('Ticket price: €15')
