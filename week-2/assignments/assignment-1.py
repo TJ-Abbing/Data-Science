3baad5811621fdad44d5d8fcc395b7697ae1c17a
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-09 14:24:17 +0200

    WIP: Assignement-1.

--- week-2/assignments/assignment-1.py
+++ week-2/assignments/assignment-1.py
@@ -1,17 +1,35 @@
-
-
 # Assignment 1 - Write a programm that sells tickets.
 
 # Ask for user's age.
 age = int(input("Enter your age: "))
-# Check if user is within the valid range of 18 to 70.
+
+# Check if user is within the invalid age range (younger than 18 or older than 70). If true (=invalid range), deny access.
 if age < 18 or age > 70:
+
     print("Sorry, for security reasons we can't sell you a ticket.")
+
+    # Check if user is younger than 25. 
 elif age < 25:
+
+    # If true, ask if user is a student & if user has a membership card.
     is_student = input("Are you a student? (Enter 'yes' or 'no'): ").lower() == 'yes'
     has_membership = input("Do you have a membership card? (Enter 'yes' or 'no'): ").lower() == 'yes'
+
+    # Check if user is both a student and member. 
     if is_student and has_membership:
+
+        # If true, check if user's age is an odd number. If true (=odd number), give user a free ticket.
         if age % 2 == 1:
             print("It's your lucky day! You've won a free ticket!")
-    if is_student or has_membership:
-            print('Ticket price: €5')
+
+        # If false (=even number), check if user is either a student or a member. If true, ticket price is €5.
+        elif is_student or has_membership:
+            print('Ticket price: €5')   
+
+        # If false (=not a student and not a member), ticket price is €10.
+        elif not is_student and not has_membership:
+            print('Ticket price: €10')
+
+# If user is older than 25, ticket price is €15.
+elif age > 25:
+    print('Ticket price: €15')
