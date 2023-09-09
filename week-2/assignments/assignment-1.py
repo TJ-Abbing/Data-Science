6b6c4dca1b91e98e7b92e531c69121d4e4e70e62
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-09 15:59:35 +0200

    Fixed; issue where user would not get ticket price when in valid age range, have an even age and be both member and/or student.

--- week-2/assignments/assignment-1.py
+++ week-2/assignments/assignment-1.py
@@ -3,16 +3,16 @@
 # Ask for user's age.
 age = int(input("Enter your age: "))
 
-# Check if user is within the invalid age range (younger than 18 or older than 70). If true (=invalid range), deny access.
+# Check if user is within the invalid age range (younger than 18 or older than 70). If true (=in invalid range), deny access.
 if age < 18 or age > 70:
 
     print("Sorry, for security reasons we can't sell you a ticket.")
 
-    # If user is older than 25, ticket price is €15.
+# If user is over 25, ticket price is €15.
 elif age >= 25:
     print('Ticket price: €15')
 
-    # Check if user is younger than 25. 
+# Check if user is younger than 25. 
 elif age < 25:
 
     # If true, ask if user is a student & if user has a membership card.
@@ -21,15 +21,18 @@ elif age < 25:
 
     # Check if false (=not a student and not a member). If false, ticket price is €10.
     if not is_student and not has_membership:
-        print('Ticket price: €10')
+        print('Ticket price: €10')  
 
     # Check if user is both a student and member. 
-    elif is_student and has_membership:
+    if is_student and has_membership:
 
-        # If true, check if user's age is an odd number. If true (=odd number), give user a free ticket.
-        if age % 2 == 1:
+        # If true, check if user's age is an odd number. If false (=even number), ticket price is €5.
+        if not age % 2 == 1:
+            print('Ticket price: €5')   
+        # If false (=odd number), give user a free ticket.
+        else:
             print("It's your lucky day! You've won a free ticket!")
 
-        # If false (=even number), check if user is either a student or a member. If true, ticket price is €5.
-        elif is_student or has_membership:
-            print('Ticket price: €5')   
+    # If false (=student and / or member), check if user is either a student or a member. If true, ticket price is €5.
+    elif is_student or has_membership:
+        print('Ticket price: €5')   
