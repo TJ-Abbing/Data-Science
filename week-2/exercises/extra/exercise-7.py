e489689c2096c622cd06e55aef25071c10e2965b
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-12 11:37:29 +0200

    Added; exercise 7 solution.

--- week-2/exercises/extra/exercise-7.py
+++ week-2/exercises/extra/exercise-7.py
@@ -6,4 +6,9 @@ The conversion formula is Celsius = (Fahrenheit - 32) * 5/9.
 Print the result. Use an if statement to check that the user input a valid number.
 """
 
-# TODO
+# Declare variable.
+toBeConverted = int(input("Enter a temperature in Fahrenheit: "))
+converted = (toBeConverted - 32) * 5/9
+
+# Print both variables.
+print(f"{toBeConverted} degrees Fahrenheit is equal to {converted} degrees Celsius.")
