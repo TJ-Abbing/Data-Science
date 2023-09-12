02b78b9f959ccd8b8115b9da9e61c5eadc6d2854
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-12 10:44:02 +0200

    Added; solution for exercise 4.

--- week-2/exercises/extra/exercise-4.py
+++ week-2/exercises/extra/exercise-4.py
@@ -5,4 +5,13 @@ If the numbers are equal, print a message saying so.
 Use comparison operators and if-else statements to accomplish this.
 """
 
-# TODO
+input_number_1 = int(input("Enter a number: "))
+input_number_2 = int(input("Enter another number: "))
+
+comparison = {
+    input_number_1 == input_number_2 : "The numbers are equal.",
+    input_number_1 > input_number_2 : "The first number is greater.",
+    input_number_1 < input_number_2 : "The second number is greater."
+}
+
+print(comparison.get(True, "Something went wrong."))
