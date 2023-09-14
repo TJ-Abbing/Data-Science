34ef252b7ddfbb80fd1eb58388eafecb4dd0c606
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-14 16:13:18 +0200

    Updated; format.

--- week-3/exercises/exercise-2.py
+++ week-3/exercises/exercise-2.py
@@ -4,6 +4,7 @@ table for numbers 1 through 10.
 """
 # Declare variables.
 res = 0 # Declare variable.
+
 for i in range(1, 11): # Loop through the range of numbers from 1 to 10.
     for j in range(1, 11): # Loop through the range of numbers from 1 to 10.
         res = i * j # Multiply i by j and store the result in the res variable.
