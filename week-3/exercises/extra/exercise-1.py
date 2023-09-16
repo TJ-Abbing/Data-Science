a157414401a6415f800177214ce5c900228378ae
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-16 14:48:16 +0200

    Added; extra exercise 1.

--- week-3/exercises/extra/exercise-1.py
+++ week-3/exercises/extra/exercise-1.py
@@ -3,4 +3,18 @@ Exercise 1:
 Write a Python program that takes a positive integer as input from the user and prints the factorial of that number.
 """
 
-# TODO
+# Declare variables
+positive_integer = int(input("Enter a positive integer (number): "))
+factorial = 1
+
+# Check if the number is negative
+if positive_integer < 0:
+    print("Sorry, factorial does not exist for negative integers. Please enter a positive integer.")
+# Check if the number is 0
+elif positive_integer == 0:
+    print("The factorial of 0 is 1.")
+# Calculate the factorial
+else:
+    for i in range(1, positive_integer + 1): 
+        factorial = factorial * i
+    print("The factorial of", positive_integer, "is", factorial)
