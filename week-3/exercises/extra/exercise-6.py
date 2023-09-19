7be62a3174ae7b0f22e9232ed0b8ffe0e7126e7d
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-19 12:28:03 +0200

    Added; extra exercise 6.

--- week-3/exercises/extra/exercise-6.py
+++ week-3/exercises/extra/exercise-6.py
@@ -7,4 +7,23 @@ HINT:
 2. test that lcm is divisible by both numbers, if not increase lcm by adding maximum number and repeat step 2
 """
 
-# TODO
+# Declare variables.
+first_integer = int(input("Enter the first integer: "))
+second_integer = int(input("Enter the second integer: "))
+
+# Find the maximum of the two integers.
+lcm = max(first_integer, second_integer)
+
+# Check if the number is negative.
+if first_integer < 0 or second_integer < 0:
+    print("Sorry, LCM does not exist for negative integers. Please enter a positive integer.")
+# Check if the number is 0.
+if first_integer == 0 or second_integer == 0:
+    print("The LCM of 0 and", second_integer, "is", second_integer)
+# Calculate the LCM.
+else:
+    while lcm > 0:
+        if lcm % first_integer == 0 and lcm % second_integer == 0:
+            break
+        lcm = lcm + 1
+    print("The LCM of", first_integer, "and", second_integer, "is", lcm)
