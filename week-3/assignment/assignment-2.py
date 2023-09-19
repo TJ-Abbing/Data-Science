398732e0ebbc87270a0e7cb01bcaee1ed8bebc2a
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-19 14:22:46 +0200

    Added; assignment 2 file.

---
+++ week-3/assignment/assignment-2.py
@@ -0,0 +1,21 @@
+"""
+Instructions
+Assignment Week 3
+Well-ordered numbers are numbers whose digits increase from left to right.
+for example, the number 1234 is well-ordered as 1 but 4321 and 122 are not well-ordered.
+Write a program that finds and reports the total number of well-ordered numbers between the given 
+range (start to end).
+1. The program should only accept numbers greater than 3 digits, so if the user enters a number 
+less than 3 digits, print an error message and ask for another number.
+2. Create an integer variable called total that holds the count of well-ordered numbers.
+3. Create a string variable called numbers_list and each time you find a well-ordered number,
+add it to this string with an extra space after.
+4. At the end, print a message like this "The count of well-ordered numbers between 1 IO and 130
+is 7 and these are the numbers: 123 124 125 126 127 128 129"
+
+Tip: You can use the % and // operators to separate digits.
+
+Note: Try to use f-string formatting that we practiced in the last session for the message you print at the end.
+• Write your code and submit your Python file.
+• You don't need to comment all the code, just the lines you think need extra explanation.
+"""
