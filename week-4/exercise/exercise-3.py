825cb4d85ce5d43d272538b74c712cf369930ac6
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-21 15:03:45 +0200

    Added; exercise 4.

--- week-4/exercise/exercise-3.py
+++ week-4/exercise/exercise-3.py
@@ -5,28 +5,27 @@ Then use that function in another function to count the number of vowels in a st
 # Declare variable.
 string = input("Enter a string: ")
 
-# Declare function. 
-def is_vowel(string):
-    # Declare variable.
-    vowels = "aeiouAEIOU"
-    # Check if string is a vowel.
-    if string in vowels:
+# Declare function.
+def is_vowel(letter):
+    # Check if letter is a vowel.
+    if letter in "aeiouAEIOUI":
+        # Return True.
         return True
-    else:
-        return False
+    # Return False.
+    return False
 
 # Declare function.
 def count_vowels(string):
     # Declare variable.
     count = 0
     # Loop through string.
-    for i in string:
-        # Check if string is a vowel.
-        if is_vowel(i):
+    for letter in string:
+        # Check if letter is a vowel.
+        if is_vowel(letter):
             # Increase count by 1.
             count += 1
     # Return count.
     return count
 
 # Call function.
-print(f'The number of vowels in "{string}" is {count_vowels(string)}.')
+print(f'There are {count_vowels(string)} vowels in "{string}".')
--- week-4/exercise/exercise-4.py
+++ week-4/exercise/exercise-4.py
@@ -1,6 +1,34 @@
 """
 Write a program that calculates the tax percentage based on gross pay, and then 
 use this function in another function to calculate net pay. Remember that in net pay,
-the employee receives 50 euroes as transportation expenses. 
-Here is the percentage scale: 0-24
+the employee receives 50 euros as transportation expenses. 
+Here is the percentage scale: 
+0-240 -> 0%
+241-480 -> 15%
+and above 481 -> 28%
 """
+
+# Declare variable.
+gross_pay = int(input("Enter the gross pay: "))
+
+# Declare function.
+def tax_percentage(gross_pay):
+    # Check if gross pay is less than or equal to 240.
+    if gross_pay <= 240:
+        return 0
+    # Check if gross pay is less than or equal to 480.
+    elif gross_pay <= 480:
+        return 15
+    # Check if gross pay is greater than 480.
+    elif gross_pay > 480:
+        return 28
+
+# Declare function. 
+def net_pay(gross_pay):
+    # Calculate net pay.
+    net_pay = gross_pay - (gross_pay * tax_percentage(gross_pay) / 100) - 50
+    # Return net pay.
+    return net_pay
+
+# Call function.
+print(f"The net pay is {net_pay(gross_pay)} euros.")
