d239cf5c00baed1b4793928542515a4046f55be5
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-24 15:29:21 +0200

    Added; week 4 - extra exercise 1.

--- week-4/exercise/extra/exercise-1.py
+++ week-4/exercise/extra/exercise-1.py
@@ -3,4 +3,22 @@ Exercises 1:
 Write a function that takes a number and returns the sum of all the numbers from 1 to that number.
 """
 
-# TODO
+# Declare variable.
+number = int(input("Enter a number: "))
+
+# Declare function.
+def sum_of_numbers(number):
+    # Check if number is positive.
+    if number < 0:
+        print("The number is not positive.")
+    # Check if number is 0.
+    elif number == 0:
+        print("The sum of numbers from 1 to 0 is 0.")
+    # Calculate sum of numbers.
+    else:
+        sum = 0
+        for i in range (1, number + 1):
+            sum += i
+        print(f"The sum of numbers from 1 to {number} is {sum}.")
+# Call function.
+sum_of_numbers(number)
