e0ae2e344b0b777619b2ea7337ed2a90e503bfe4
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-09-24 15:48:21 +0200

    Added; week 4 - extra exercise 2.

--- week-4/exercise/extra/exercise-2.py
+++ week-4/exercise/extra/exercise-2.py
@@ -3,4 +3,26 @@ Exercises 2:
 Write a function that takes a number and returns the sum of all the odd numbers from 1 to that number.
 """
 
-# TODO
+# Declare variable.
+number = int(input("Enter a number: "))
+
+# Declare function.
+def sum_odd_numbers(number):
+    # Check if number is negative.
+    if number < 0:
+        print("Number must be positive.")
+    # Check if number is 0.
+    elif number == 0:
+        print("Can't be equal to 0.")
+    # Calculate sum of odd numbers.
+    else:
+        # Check if number is even.
+        if number % 2 == 0:
+            return
+        else:
+            sum = 0
+            for i in range (1, number + 1, 2):
+                sum += i
+            print(f"The sum of odd numbers from 1 to {number} is {sum}.")
+# Call function.
+sum_odd_numbers(number)
