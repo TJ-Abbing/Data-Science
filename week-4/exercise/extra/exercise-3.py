efafb52d2effe4fbd23929b20d6a01fc7e1c1c05
TJ-Abbing <103822032+TJ-Abbing@users.noreply.github.com>
2023-10-04 14:51:10 +0200

    Added; week 4 extra exercise 3.

--- week-4/exercise/extra/exercise-3.py
+++ week-4/exercise/extra/exercise-3.py
@@ -3,4 +3,26 @@ Exercises 3:
 Write a function that takes a number and returns True when it is a prime number, otherwise False.
 """
 
-# TODO
+# Declare variable.
+number = int(input("Enter a number: "))
+
+# Declare function.
+def is_prime(number):
+    # Check if number is negative.
+    if number < 0:
+        print("Number must be positive.")
+    # Check if number is 0.
+    if number == 0:
+        print("Number can't be equal to 0.")
+    # Check if number is 1.
+    if number == 1:
+        print("Number can't be equal to 1.")
+    # Check if number is prime.
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+        return True	
+    
+# Call function.
+print(is_prime(number))
